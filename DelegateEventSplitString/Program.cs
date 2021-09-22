3a26c6c5acf110b16844a5bc380a38e16eb4088c
hxchua00 <hxchua00@gmail.com>
2021-09-23 01:08:01 +0800

    Add project files.

---
+++ DelegateEventSplitString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateEventSplitString
+{
+    public delegate void SplitStringEventHandler(string msg);
+    class Program
+    {
+        public static event SplitStringEventHandler NewSplit;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string here: ");
+            string input = Console.ReadLine();
+            Console.WriteLine();
+
+            StringToSplit split = new StringToSplit(input);
+
+            NewSplit += split.SplitStringCount;
+            NewSplit.Invoke(input);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DelegateEventSplitString/StringToSplit.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateEventSplitString
+{
+    class StringToSplit
+    {
+        string message;
+        string[] splitStr;
+        public StringToSplit(string message)
+        {
+            this.message = message;
+        }
+
+        public void SplitStringCount(string msg)
+        {
+            splitStr = msg.Split(' ', '.', ',', '?', '!');
+
+            Console.WriteLine("Number of words is " + splitStr.Length);
+            Console.WriteLine();
+
+            foreach (var s in splitStr)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine();
+        }
+    }
+}
